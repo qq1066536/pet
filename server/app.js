9ce4bdef4565860b94859a289739334424d1932e
jq <2716271743@qq.com>
2019-01-07 23:24:44 +0800

    提交订单路由

--- server/app.js
+++ server/app.js
@@ -9,6 +9,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var supplierRouter = require("./routes/supplier")
 var platformRouter = require('./routes/platform.js')
+var ordersRouter = require('./routes/orders');
+
+
 var app = express();
 
 // view engine setup
@@ -25,6 +28,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/supplier",supplierRouter)
 app.use('/platform', platformRouter);
+app.use('/orders', ordersRouter);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
---
+++ server/routes/orders.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var router = express.Router();
+const client = require("ykt-http-client");
+client.url("127.0.0.1:8080");
+
+/* GET users listing. */
+/* router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+}); */
+/* router.get("/", async function (req, res) {
+    let data = await client.get("/order");
+    res.send(data);
+}) */
+// 根据门店id查询订单
+router.get("/:id", async function (req, res) {
+    let id = req.params.id;
+    let data = await client.get("/order/" + id, {
+        submitType: "findJoin"
+        , ref: "shop"
+    });
+    res.send(data)
+})
+
+// 根据条件查询订单
+router.get("/", async function (req, res) {
+    let { page, rows, value, type } = req.query;
+    let data = await client.get("/order", {
+        page,
+        rows,
+        findType: "exact",
+        submitType: "findJoin", ref: "order",
+    });
+    // console.log(type,value) undefined undefined
+    if (type) {
+        let isCludes;
+        for (let i = 0; i < data.rows.length; i++) {
+            isCludes = _.includes(data.rows[i].order[type], value); // 数据中是否包含value
+            if (!isCludes) {
+                data.rows.splice(i, 1);
+                i--;
+            }
+        }
+        // 总共的条数
+        if (value != '') {
+            data.total = data.rows.length;
+        }
+        // 当前页
+        data.curpage = parseInt(data.total / data.eachpage) || 1;
+    }
+    res.send(data);
+})
+
+//修改
+router.put("/:id", async function (req, res) {
+    let id = req.params.id; // 订单的id
+    let { user, addr, phone } = req.body;
+    let data = await client.put("/order/" + id, {
+        user, addr, phone
+    });
+    res.send({ status: 1 });
+})
+
+module.exports = router;

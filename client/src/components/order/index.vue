dfcb8099f8054928da1251689958f9ba51057262
jq <2716271743@qq.com>
2019-01-08 14:44:27 +0800

    提交store

---
+++ client/src/components/order/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <OrderUpdate></OrderUpdate>
+    <OrderSearch></OrderSearch>
+    <OrderList></OrderList>
+    <OrderPage></OrderPage>
+  </div>
+</template>
+
+<script>
+import OrderList from "./OrderList";
+import OrderPage from "./OrderPage";
+import OrderUpdate from "./OrderUpdate";
+import OrderSearch from "./OrderSearch";
+import axios from "axios";
+export default {
+  components: {
+    OrderList,
+    OrderPage,
+    OrderUpdate,
+    OrderSearch
+  },
+  data() {},
+  created: function() {
+    this.getOrder();
+  }
+};
+</script>
+
+<style>
+</style>

8f8623d6c1f67a431b739e18073629e4e3735e81
HS <1227003192@qq.com>
2019-01-08 12:33:17 +0800

    路由

---
+++ server/routes/shop.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+const client = require("ykt-http-client");
+client.url("127.0.0.1:8080");
+//注册门店
+router.post('/', async function (req, res) {
+    let { name, addr, tel, business_lic, desc, business_no, location, city, legal_person, tel, img_head,feature,vip,package,stuff,services,user} = req.body;
+    location = location && JSON.parse(location);
+    stuff = stuff && JSON.parse(stuff);
+    client.post('/shop', { name, addr, tel, business_lic, desc, products, business_no, location, city, legal_person, tel, img_head,feature,vip,package,stuff,services,user})
+    res.send({ status: 1 });
+  });
+  //查找门店
+  router.get('/:id', async function (req, res) {
+    let id = req.params.id;
+    let data = await client.get("/shop/" + id);
+    res.send(data);
+  })
+module.exports = router;

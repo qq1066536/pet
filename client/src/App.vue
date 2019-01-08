8b425b6b4efada03fbabb1448b058e2553325eca
alex liu <1066536184@qq.com>
2019-01-08 15:23:33 +0800

    1

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+<router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+}
+</script>
+
+<style>
+#app,html,body{
+  height: 100%;
+  margin: 0;
+}
+</style>
---
+++ client/src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Manage from "../components/manage"
+import UserManage from "../components/platform/UserManage"
+import PetmasterManage from "../components/platform/PetmasterManage"
+import StoreManage from "../components/platform/StoreManage"
+import Statistics from "../components/platform/Statistics"
+import SupProducts from '../components/supplier/products'
+Vue.use(VueRouter);
+export default new VueRouter({
+    routes: [
+        {
+            path: '/manage',
+            component: Manage,
+            children: [{
+                path: "/manage/platform/UserManage",
+                component: UserManage
+            },
+            {
+                path: "/manage/platform/PetmasterManage",
+                component: PetmasterManage
+            },
+            {
+                path: "/manage/platform/StoreManage",
+                component: StoreManage
+            },
+            {
+                path: "/manage/platform/Statistics",
+                component: Statistics
+            },{
+                path:"/manage/supplier/products",
+                component:SupProducts
+            }
+            ]
+        }
+    ],
+    mode: 'history'
+})

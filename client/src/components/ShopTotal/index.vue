ad09bfa0169463cf1efbaea319f70285f3df6e5a
jq <2716271743@qq.com>
2019-01-10 23:16:26 +0800

    clear

--- client/src/components/ShopTotal/index.vue
+++ client/src/components/ShopTotal/index.vue
@@ -1,16 +1,20 @@
 <template>
+  <div>
     <Numecharts></Numecharts>
+    <Salesecharts></Salesecharts>
+  </div>
 </template>
 
 <script>
 import Numecharts from "./Numecharts";
+import Salesecharts from "./Salesecharts";
 export default {
-    components: {
-        Numecharts,
-    }
-}
+  components: {
+    Numecharts,
+    Salesecharts
+  }
+};
 </script>
 
 <style>
-
 </style>

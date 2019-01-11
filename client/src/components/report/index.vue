87dd3b8ca5853e9589137faff03e701ecc05793c
jq <2716271743@qq.com>
2019-01-11 16:41:52 +0800

    修改文件同名

--- client/src/components/report/index.vue
+++ client/src/components/report/index.vue
@@ -1,23 +1,5 @@
 <template>
-  <div>
-    <el-submenu>
-      <template slot="title">
-        <i class="el-icon-tickets"></i>
-        <span>报表展示</span>
-      </template>
-      <el-menu-item-group>
-        <el-menu-item index="/manage/report/salenum">
-          <i class="el-icon-menu"></i>
-          <span slot="title">销售量统计</span>
-        </el-menu-item>
-        <el-menu-item index="/manage/report/sales">
-          <i class="el-icon-menu"></i>
-          <span slot="title">销售额统计</span>
-        </el-menu-item>
-      </el-menu-item-group>
-    </el-submenu>
-    <router-view></router-view>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
@@ -25,17 +7,4 @@ export default {};
 </script>
 
 <style scoped>
-.headerBgc {
-  background-color: skyblue;
-}
-.container {
-  height: 100%;
-}
-.menu {
-  height: 100%;
-}
-.user {
-  float: right;
-  font-size: 16px;
-}
 </style>

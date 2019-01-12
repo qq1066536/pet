fadf8fb1c4f6bd5ea1ed4b392b6b8d8891b864fe
jq <2716271743@qq.com>
2019-01-12 23:02:37 +0800

    统计报表

--- client/src/components/report/SaleNum/index.vue
+++ client/src/components/report/SaleNum/index.vue
@@ -1,35 +1,55 @@
 <template>
-  <div id="NumChart" :style="{width: '300px', height: '300px'}">销量统计</div>
+  <!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
+  <div>
+    <div id="saleNum" style="width: 600px;height:400px;"></div>
+  </div>
 </template>
 
 <script>
-import echarts from 'echarts'
+import echarts from "echarts";
 export default {
-  computed() {
-    this.showEcharts()
+  data() {
+    return {};
+  },
+  mounted() {
+    this.haha();
   },
   methods: {
-    showEcharts() {
+    haha() {
       // 基于准备好的dom，初始化echarts实例
-      var myChart = echarts.init(document.getElementById("NumChart"));
+      var myChart = echarts.init(document.getElementById("saleNum"));
+
       // 指定图表的配置项和数据
       var option = {
         title: {
-          text: "ECharts 入门示例"
+          text: "店铺销量统计"
         },
         tooltip: {},
         legend: {
           data: ["销量"]
         },
         xAxis: {
-          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+          data: [
+            "1月",
+            "2月",
+            "3月",
+            "4月",
+            "5月",
+            "6月",
+            "7月",
+            "8月",
+            "9月",
+            "10月",
+            "11月",
+            "12月"
+          ]
         },
         yAxis: {},
         series: [
           {
             name: "销量",
             type: "bar",
-            data: [5, 20, 36, 10, 10, 20]
+            data: [5, 20, 36, 10, 10, 20, 5, 20, 36, 10, 10, 20]
           }
         ]
       };

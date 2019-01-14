a1177ffa093b7ef2f2373e3ef1bbc623c07c6042
jq <2716271743@qq.com>
2019-01-14 09:35:28 +0800

    123

--- client/src/components/report/SaleNum/index.vue
+++ client/src/components/report/SaleNum/index.vue
@@ -11,6 +11,15 @@
 
 <script>
 import echarts from "echarts/lib/echarts";
+// 引入柱状图
+import "echarts/lib/chart/bar"
+import "echarts/lib/chart/pie";
+import "echarts/lib/chart/scatter";
+// 引入提示框和标题组件
+import "echarts/lib/component/tooltip";
+import "echarts/lib/component/title";
+import "echarts/lib/component/legend";
+import "echarts/extension/bmap/bmap";
 export default {
   // 指定图表的配置项和数据
   data() {
